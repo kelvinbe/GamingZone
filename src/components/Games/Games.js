e4f6873e2efb2a29b98c717e030b159a0049914e
kelvinbe <kelvin.beno.k@gmail.com>
2023-08-25 09:48:07 +0300

    updates

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -13,172 +13,185 @@ import ShareIcon from "@mui/icons-material/Share";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Header from "../Header/Header";
 import "../Games/Games.css";
-import LoaderPage from "../Loader/LoaderPage";
 import { Grid } from "@mui/material";
-import {useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 import OrderMenus from "../Menu/OrderMenu";
 import PlatformsMenus from "../Menu/PlatformsMenu";
-import { GetAllGames} from "./GamesApi";
+import { GetAllGames } from "./GamesApi";
 import FooterContainer from "../Footer/footer";
-
-
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Games = () => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [isHover, setIsHover] = useState(false)
-  const [isGameId, setGameId] = useState(null)
-  const [index, setIndex] = useState(1)
+  const [isLoading, setIsLoading] = useState(false);
+  const [isHover, setIsHover] = useState(false);
+  const [isGameId, setGameId] = useState(null);
+  const [index, setIndex] = useState(1);
 
-  const dispatch = useDispatch()
-  const results = useSelector((state) => state.results )
+  const dispatch = useDispatch();
+  const results = useSelector((state) => state.results);
   const stop = 7;
 
-
   const handleHover = (id) => {
-    setIsHover(true)
-    setGameId(id)
-
-
-  }
+    setIsHover(true);
+    setGameId(id);
+  };
 
   const handleHoverOut = () => {
-
-    setIsHover(false)
-
-}
-
-useEffect(() => {
-  let interval = null
-  function increment() {
-    setIndex(index + 1);;
-    if (index === stop) {
-      setIndex(1)
-    }
-  
-  }
-
-
-  if(isHover){
-  interval = setInterval(increment, 5000);
-  }else if(!isHover){
-    setIndex(1)
-    clearInterval(interval)
-  }
-
-  return () => clearInterval(interval)
-
-}, [isHover, index])
-
-
+    setIsHover(false);
+  };
 
   useEffect(() => {
-    
-    
-    const FetchData = async () => {
+    let interval = null;
+
+    function increment() {
+      setIndex((prevIndex) => (prevIndex === stop ? 1 : prevIndex + 1));
+    }
+
+    if (isHover) {
+      interval = setInterval(increment, 5000);
+    } else {
+      setIndex(1);
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval); // Clean up the interval when unmounting
+  }, [isHover]);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchData = async () => {
       try {
-        setIsLoading(true)
-        const resp = await GetAllGames()
-        dispatch({type: 'GET_RESULTS', data: resp})
-        setIsLoading(false)
+        setIsLoading(true);
+        const resp = await GetAllGames();
+        
+        // Only update state if the component is still mounted
+        if (isMounted) {
+          dispatch({ type: "GET_RESULTS", data: resp });
+          setIsLoading(false);
+        }
       } catch (err) {
-        setIsLoading(false)
+        setIsLoading(false);
         console.error(err);
       }
-    }
-    FetchData() 
-  
+    };
 
+    fetchData();
+
+    return () => {
+      isMounted = false; // Mark the component as unmounted
+    };
   }, [dispatch]);
 
   if (!results) return null;
 
   console.log(results);
 
-return (
-    <div>
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        backgroundColor: "maroon",
-        paddingTop: 80,
-        paddingLeft: 20,
-        paddingRight: 20,
-      }}
-    >
-    { isLoading ? <LoaderPage /> :  (
-      <>
-    <Header />
+  return (
+    <div style={{ height: "100vh", backgroundColor: "maroon"}}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          backgroundColor: "maroon",
+          paddingTop: 80,
+          paddingLeft: 20,
+          paddingRight: 20,
+        }}
+      >
+        <Header />
 
-      <Grid container style={{ justifyContent: "center" }}>
-        <Grid item md={12}>
-          <h1 className="text-games">Top Upcoming Games</h1>
+        <Grid container style={{ justifyContent: "center" }}>
+          <Grid item md={12}>
+            <h1 className="text-games">Top Upcoming Games</h1>
+          </Grid>
+          <Grid
+            item
+            md={12}
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            <OrderMenus />
+            <PlatformsMenus />
+          </Grid>
+          {isLoading ? (
+            <CircularProgress />
+          ) : (
+            <>
+              {results?.map((game, key) => {
+                return (
+                  <Grid
+                    item
+                    style={{ padding: 10 }}
+                    key={game.id}
+                    className="flex-wrapper"
+                  >
+                    <Card
+                      sx={{ maxWidth: 345, width: 354 }}
+                      key={game.id}
+                      className="tag"
+                      onMouseOver={() => handleHover(game.id)}
+                      onMouseOut={handleHoverOut}
+                    >
+                      <CardHeader
+                        avatar={
+                          <Avatar
+                            sx={{ bgcolor: red[500] }}
+                            aria-label="recipe"
+                          >
+                            R
+                          </Avatar>
+                        }
+                        action={
+                          <IconButton aria-label="settings">
+                            <MoreVertIcon />
+                          </IconButton>
+                        }
+                        title={game.name}
+                        subheader={game.released}
+                      />
+                      {isHover && game.id === isGameId ? (
+                        <CardMedia
+                          component="img"
+                          height="194"
+                          image={game.short_screenshots[index].image}
+                          alt="Paella dish"
+                        />
+                      ) : (
+                        <CardMedia
+                          component="img"
+                          height="194"
+                          image={game.background_image}
+                          alt="Paella dish"
+                        />
+                      )}
+                      <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                          genre: {game.genres[0].name}
+                          <br />
+                          Rating: {game.rating}
+                          <br />
+                          Tags: {game.tags[0].name}
+                        </Typography>
+                      </CardContent>
+                      <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                          <FavoriteIcon />
+                        </IconButton>
+                        <IconButton aria-label="share">
+                          <ShareIcon />
+                        </IconButton>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                );
+              })}
+            </>
+          )}
         </Grid>
-        <Grid item md={12} style={{display: 'flex', justifyContent: 'center'}}>
-          <OrderMenus />
-          <PlatformsMenus />
-
-        </Grid>
-        {results?.map((game, key) => {
-          return (
-            <Grid item style={{ padding: 10 }} key={game.id} className="flex-wrapper">
-              <Card sx={{ maxWidth: 345, width: 354 }} key={game.id} className="tag" onMouseOver={() => handleHover(game.id)} onMouseOut={handleHoverOut}>
-                <CardHeader
-                  avatar={
-                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                      R
-                    </Avatar>
-                  }
-                  action={
-                    <IconButton aria-label="settings">
-                      <MoreVertIcon />
-                    </IconButton>
-                  }
-                  title={game.name}
-                  subheader={game.released}
-                />
-          {isHover && game.id === isGameId ? <CardMedia
-                  component="img"
-                  height="194"
-                  image={game.short_screenshots[index].image}
-                  alt="Paella dish"
-                /> : <CardMedia
-                component="img"
-                height="194"
-                image={game.background_image}
-                alt="Paella dish" />}
-                <CardContent>
-                  <Typography variant="body2" color="text.secondary">
-                    genre: {game.genres[0].name}
-                    <br />
-                    Rating: {game.rating}
-                    <br />
-                    Tags: {game.tags[0].name}
-                  </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                  <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                  </IconButton>
-                  <IconButton aria-label="share">
-                    <ShareIcon />
-                  </IconButton>
-                </CardActions>
-              </Card>
-            </Grid>
-          );
-        })}
-      </Grid>
-      </>
-      )}
-
-    </div>
-    {!isLoading && <FooterContainer />}
+      </div>
+      {!isLoading && <FooterContainer />}
     </div>
   );
-}
+};
 
-
-
-
-export default Games
+export default Games;
