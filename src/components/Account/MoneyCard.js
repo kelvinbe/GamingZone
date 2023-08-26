974e836ac8a0ced58a48c527a61f6581034f9f79
kelvinbe <kelvin.beno.k@gmail.com>
2023-08-26 17:14:04 +0300

    updates

--- src/components/Account/MoneyCard.js
+++ src/components/Account/MoneyCard.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Paper from "@mui/material/Paper";
 import { Typography } from "@mui/material";
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+
+
 
 const MoneyCard = ({amount, name}) => {
   return (
     <div>
-      <Paper
+    <Paper
         elevation={6}
         style={{
           width: "200px",
@@ -13,14 +17,16 @@ const MoneyCard = ({amount, name}) => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          flexDirection:'column'
+          margin: 20
         }}
-      >
+    >
+        <div style={{flexDirection: 'column'}}>
         <div>
-           <Typography variant="h5"> {name}</Typography>
+        <Typography variant="h5"> {name}</Typography>
         </div>
         <div>
         <Typography variant="h5">{amount} $</Typography>
+        </div>
         </div>
       </Paper>
     </div>
--- src/components/Account/MoneyMade.js
+++ src/components/Account/MoneyMade.js
@@ -1,12 +1,16 @@
 import React from "react";
 import MoneyCard from "./MoneyCard";
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 
 const Data = [{ name: 'ps5', amount: '30,000'}, { name: 'xbox', amount: '20,000'}, { name: 'psp', amount: '10,000'}, { name: 'pc', amount: '50,000'}];
 
 const MoneyMade = () => {
+const matches = useMediaQuery('(max-width:480px)');
+
   return (
-    <div style={{height: 200}}>
-      <div style={{display: 'flex', justifyContent: 'space-between'}}>
+    <div style={{height: matches ? 600 : 200}}>
+      <div style={{display: 'flex', justifyContent: 'space-between', flexDirection: matches ? 'column' : 'row', alignItems: 'center'}}>
         {Data.map((game) => {
           return <MoneyCard amount={game.amount} name={game.name} />;
         })}
